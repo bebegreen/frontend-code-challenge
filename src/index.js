0a1e0dbfbecb0be0256554c2489f90342f03e58e
bebegreen <binyamin.green@gmail.com>
2018-02-02 15:53:52 +0200

    react setup

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import css from './static/css/globals.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App';
+
+ReactDOM.render(
+    <App />
+  , document.querySelector('.main'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,40 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
-    filename: './bundle.js',
-    path: path.join(__dirname, 'dist'),
+    filename: 'static/js/app.bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
+      { test:/\.js$/, use: 'babel-loader' , exclude: /node_modules/ },
+    //  { test:/\.css$/, use: ['style-loader','css-loader'] },
       {
         test: /\.css$/,
-        use: ['style-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
       },
-    ],
+    ]
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new ExtractTextPlugin("static/css/globals.css"),
+    new CopyWebpackPlugin([
+          { from: 'src/index.html' },
+          { from: 'src/static/icons' , to: 'static/icons'},
+      ])
   ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 3000,
+    stats:"minimal",
+    open:true,
+    openPage:""
+  },
+  devtool:"source-map"
 };
